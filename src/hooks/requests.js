f6acbaf3a3de8918d20737536134fcaa0e9a17c9
enrique95 <enrique_alvarado95@hotmail.com>
2023-08-02 15:44:56 -0600

    added env variable in requests

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -1,6 +1,6 @@
-const DEV_API_URL = 'http://localhost:8000';
+const DEV_API_URL = process.env.REACT_APP_DEV_API_URL;
 
 export const httpGetAllUsers = async () => {
-  const response = await fetch(`${DEV_API_URL}/administration/users`);
+  const response = await fetch(`${DEV_API_URL}administration/users`);
   return await response.json();
 };
