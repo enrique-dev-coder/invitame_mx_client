5dcbb497d620c290d6c440e2199984f01789a642
eli castellano <eliman1926@gmail.com>
2023-08-03 13:36:06 -0400

    Probando

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import 'assets/scss/style.scss';
 import config from './config';
 
 // ==============================|| REACT DOM RENDER  ||============================== //
+// AQUI ESTUVO ELI
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
