8361c34ec6aa92111ae2423535caf63aac81804e
eli castellano <eliman1926@gmail.com>
2023-08-26 20:42:23 -0400

    Back to initial state of the branch

--- src/ui-component/cards/MainCard.js
+++ src/ui-component/cards/MainCard.js
@@ -5,7 +5,7 @@ import { forwardRef } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
 
-// constant
+// constant.
 const headerSX = {
   '& .MuiCardHeader-action': { mr: 0 }
 };
