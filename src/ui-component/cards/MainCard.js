b6447a6a54c36a861bdeb89fae84f35148547d99
eli castellano <eliman1926@gmail.com>
2023-09-08 12:54:03 -0400

    Add icon search to input in guests

--- src/ui-component/cards/MainCard.js
+++ src/ui-component/cards/MainCard.js
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { Card, CardContent, CardHeader, Divider, TextField, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Divider, Typography } from '@mui/material';
 
 // constant.
 const headerSX = {
@@ -51,7 +51,6 @@ const MainCard = forwardRef(
               <Typography variant="h3">{title}</Typography> : 
               <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <h4>{title}</h4>
-                <TextField id="standard-basic" variant="standard" color='secondary' />
               </div>
             } 
           action={secondary} />}
